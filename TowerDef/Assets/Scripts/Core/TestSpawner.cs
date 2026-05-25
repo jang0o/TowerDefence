d9365cd0c6222de4242a8a01404e89f195ba99df
KruhaPirat <125740997+NoFear163@users.noreply.github.com>
2026-05-25 13:27:59 +0400

    Update TestSpawner.cs

--- TowerDef/Assets/Scripts/Core/TestSpawner.cs
+++ TowerDef/Assets/Scripts/Core/TestSpawner.cs
@@ -3,18 +3,14 @@ using UnityEngine;
 public class TestSpawner : MonoBehaviour
 {
     [Header("��������� �����")]
-    // ������ ����� ��� BaseEnemy, ������� �� ������� ��� 3D
     public BaseEnemy enemyToTest;
 
-    // ������ �� ����� ���� (Path_Main)
     public Waypoints pathToFollow;
 
     void Start()
     {
-        // ���������, �� �� �� ���������� � ����������
         if (enemyToTest != null && pathToFollow != null)
         {
-            // �������� ����� �������
             enemyToTest.SetupPath(pathToFollow);
             Debug.Log("���� �������: ���� ����� �������� �� ���� " + pathToFollow.name);
         }
